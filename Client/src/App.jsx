99add455dac48d889ef3317c771f517c1b6d11bb
spencer4hyre <163073659+spencer4hyre@users.noreply.github.com>
2024-07-11 21:09:23 -0400

    theme

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,21 @@
 //import { useState } from 'react'
-import './App.css'
+
 
 function App() {
 
 
   return (
     <>
+    <h1>Hello World</h1>
+    <button>clickclick</button>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.
+    Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis 
+    ipsum. Praesent mauris. Maecenas in magna mollis dolor sollicitudin 
+    tempor. Cras ultricies mi eu turpis hendrerit fringilla
+    </p>
+    <a>Here is a link to nowhere</a>
     </>
   )
 }
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#cfd8dc',
+      main: '#eceff1',
+      dark: '#b0bec5',
+      contrastText: '#90a4ae',
+    },
+    secondary: {
+      light: '#fafafa',
+      main: '#f5f5f5',
+      dark: '#eeeeee',
+      contrastText: '#e0e0e0',
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
